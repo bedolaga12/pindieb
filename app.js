2a186269c915f4d59478931f59520e70b7c14172
bedolaga12 <151952816+bedolaga12@users.noreply.github.com>
2024-05-11 21:29:13 +0300

    обработка файлов

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const PORT = 3005;
 
 const fs = require('fs').promises;
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', async (req, res) => {
-  const data = await fs.readFile("./public/index.html", "utf-8");
-  res.header('Content-Type', "text/html");
-  res.send(data);
-})
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
