21672ccae74a89d7da963a3f1135b8e546ca2fa9
bedolaga12 <151952816+bedolaga12@users.noreply.github.com>
2024-05-11 23:49:15 +0300

    началось

--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,50 @@
-const gamesRouter = require('express').Router();
+ const gamesRouter = require("express").Router(); 
+ const { readData, writeData } = require("../utils/data"); 
+     
+     const getAllGames = async (req, res) => {
+       const games = await readData("./data/games.json");
+       if (!games) {
+         res.status(400);
+         res.send({
+           status: "error",
+           message: "Нет игр в базе данных. Добавьте игру."
+         });
+         return;
+       }
+       req.games = games;
+       res.send(req.games);
+     };
+     
+     const deleteGame = async (req, res) => {
+       const games = await readData("./data/games.json");
+       if (!games) {
+         res.status(400);
+         res.send({
+           status: "error",
+           message: "Нет игр в базе данных. Добавьте игру.",
+         });
+         return;
+       }
+       req.games = games;
+       
+         const id = Number(req.params.id);
+         
+         req.game = req.games.find((item) => item.id === id);
+         
+         const index = req.games.findIndex((item) => item.id === req.game.id);
+         
+         req.games.splice(index, 1);      
 
-gamesRouter.get('/games', (req, res) => {
-    res.send([]);
-  });
-  
-  gamesRouter.post('/games', (req, res) => {
-    console.log(req.body);
-    res.send([]);
-  });
-  
-  module.exports = gamesRouter;
+         await writeData("./data/games.json", req.games);
+
+         res.send({
+         games: req.games,
+         updated: req.game
+       });
+     }
+     
+     gamesRouter.get("/games", getAllGames)
+     gamesRouter.delete("/games/:id", deleteGame);
+     
+     module.exports = gamesRouter;
+     
