a8ba97601d52094ff3323868aff736365d8cab66
bedolaga12 <151952816+bedolaga12@users.noreply.github.com>
2024-05-11 21:43:28 +0300

    +body-parser

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const PORT = 3005;
 const fs = require('fs').promises;
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/games', (req, res) => {
+  res.send([]);
+});
+
+app.post('/games', (req, res) => {
+  console.log(req.body);
+  res.send([]);
+});
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
